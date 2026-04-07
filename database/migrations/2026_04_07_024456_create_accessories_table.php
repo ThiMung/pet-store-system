b0d212b5a0f5d4704654329fbc0ed25328e73f82
Nhuw29 <nhu2911206@gmail.com>
2026-04-07 10:10:33 +0700

    Hoan thanh cau truc 8 bang Database va Migration cho PetShop

---
+++ database/migrations/2026_04_07_024456_create_accessories_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('accessories', function (Blueprint $table) {
+            $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
+            $table->string('accessory_type')->nullable();
+            $table->string('brand')->nullable();
+            $table->string('material')->nullable();
+            $table->string('size')->nullable();
+            $table->primary('product_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('accessories');
+    }
+};
